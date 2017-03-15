ecd48232013d7cf0b91b58e855e86060c42a970b
siphokazi-codex <aplenis@webmail.co.za>
2017-03-15 15:00:48 +0200

    My second changes on registration number webpage

--- registration_numbers.js
+++ registration_numbers.js
@@ -4,7 +4,7 @@ var txtReg = document.querySelector('.myText');
 function getRegistration () {
 if (txtReg.value.length > 0) {
   var node = document.createElement('li');
-  var textNode = document.createTextNode(txtReg.value);
+  var textNode = document.createTextNode(txtReg.value.substr(0,2).toUpperCase() + txtReg.value.substr(1).toLowerCase());
   node.appendChild(textNode);
   document.getElementById('list').appendChild(node);
   txtReg.value = "";
